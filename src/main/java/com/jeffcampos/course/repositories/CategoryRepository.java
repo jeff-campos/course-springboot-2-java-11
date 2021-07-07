d983cd2c3c31dd0f92622c22f79220965d663d1d
Admin <admin@jeff-campos.local>
2021-07-07 18:44:22 -0300

    Category

---
+++ src/main/java/com/jeffcampos/course/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.jeffcampos.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jeffcampos.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {}
